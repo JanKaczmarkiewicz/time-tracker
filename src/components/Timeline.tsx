6fee2bb497ceb6bbbbd53e6c7a58350937402c3b
JanKaczmarkiewicz <positiveKiddo@gmail.com>
2021-08-05 20:40:05 +0200

    feat: add styled components

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,7 +1,19 @@
 import TimelineSvg from "../assets/timeline-background.svg";
+import styled from "styled-components";
+import colors from "../theme/color";
+
+const TimelineWrapper = styled.div`
+  padding: 32px;
+  border: 1px solid ${colors.gray};
+  border-radius: 20px;
+`;
 
 const Timeline = () => {
-  return <img src={TimelineSvg} alt="timeline" />;
+  return (
+    <TimelineWrapper>
+      <img src={TimelineSvg} alt="timeline" />
+    </TimelineWrapper>
+  );
 };
 
 export default Timeline;
---
+++ src/theme/color.ts
@@ -0,0 +1,5 @@
+const colors = {
+  gray: "#a9a9a9",
+};
+
+export default colors;
